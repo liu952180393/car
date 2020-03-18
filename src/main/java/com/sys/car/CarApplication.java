9e80601b4311dd7044ddba5611b2d0b940445f02
liu952180393 <952180393@qq.com>
2020-03-18 13:19:31 +0800

    测试

--- src/main/java/com/sys/car/CarApplication.java
+++ src/main/java/com/sys/car/CarApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarApplication {
-
+    //启动类
     public static void main(String[] args) {
         SpringApplication.run(CarApplication.class, args);
     }
