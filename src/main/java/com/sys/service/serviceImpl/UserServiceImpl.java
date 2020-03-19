4832fa6fa0f3fe79dfa2bd32c7c7cd9486d8fdc1
liu952180393 <952180393@qq.com>
2020-03-19 16:49:49 +0800

    sql

--- src/main/java/com/sys/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/sys/service/serviceImpl/UserServiceImpl.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+/**
+ * @author liu
+ */
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
